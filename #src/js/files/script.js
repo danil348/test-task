4b33586144b0f9db9154ec732f255e0f1ae04da6
danil348 <eliseevdanil348@gmail.com>
2023-04-15 13:37:15 +0300

    закончена работа над слайдером

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,4 +1,24 @@
 window.onload = function () {
+	let slidesPerView = 4
+	if(window.innerWidth < 768){
+		slidesPerView = 2.5
+	}else if(window.innerWidth < 1024){
+		slidesPerView = 3
+	}
+
+	new Swiper('.swiper', {
+		direction: 'horizontal',
+		loop: true,
+		slidesPerView: slidesPerView,
+		pagination: {
+			el: '.swiper-pagination'
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
 	header_scroll()
 	scroll_animations()
 	icon_menu()
